ba746ce4cf6305da88754e2725b172db9417d07e
Nevo David <me@nevos.io>
2025-08-08 02:16:27 +0700

    feat: fix shorlinking

--- libraries/nestjs-libraries/src/short-linking/short.link.service.ts
+++ libraries/nestjs-libraries/src/short-linking/short.link.service.ts
@@ -37,8 +37,7 @@ export class ShortLinkService {
     }
 
     const mergeMessages = messages.join(' ');
-    const urlRegex =
-      /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))/gm;
+    const urlRegex = /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))/gm;
     const urls = mergeMessages.match(urlRegex);
     if (!urls) {
       // No URLs found, return the original text
@@ -62,8 +61,7 @@ export class ShortLinkService {
         .replace(/&num;/g, '#');
     });
 
-    const urlRegex =
-      /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))/gm;
+    const urlRegex = /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))/gm;
     return Promise.all(
       messages.map(async (text) => {
         const urls = uniq(text.match(urlRegex));
