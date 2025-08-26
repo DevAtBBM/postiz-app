138683a22d9b31796117e332c57694f1e9bad356
Nevo David <me@nevos.io>
2025-08-26 23:12:56 +0700

    feat: disconnect prisma

--- libraries/nestjs-libraries/src/database/prisma/prisma.service.ts
+++ libraries/nestjs-libraries/src/database/prisma/prisma.service.ts
@@ -1,10 +1,10 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     const connectionString = process.env.DATABASE_URL;
     const pool = new Pool({ connectionString });
@@ -23,6 +23,10 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
 }
 
 @Injectable()
@@ -32,7 +36,6 @@ export class PrismaRepository<T extends keyof PrismaService> {
     this.model = this._prismaService;
   }
 }
-
 
 @Injectable()
 export class PrismaTransaction {
