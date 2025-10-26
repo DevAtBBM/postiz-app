af9bfffeb9420841e5d6e4da8d657ad07da97f04
Nevo David <me@nevos.io>
2025-10-27 01:30:27 +0700

    fix: copilot payload

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -55,7 +55,7 @@ async function bootstrap() {
     })
   );
 
-  app.use('/copilot', (req: any, res: any, next: any) => {
+  app.use('/copilot/*', (req: any, res: any, next: any) => {
     json({ limit: '50mb' })(req, res, next);
   });
 
