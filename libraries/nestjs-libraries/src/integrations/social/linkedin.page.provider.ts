7a53d382f843b84395cefe96b26179188f670d4a
Nevo David <me@nevos.io>
2025-12-04 14:12:33 +0700

    fix: refresh token unified, and found some bugs

--- libraries/nestjs-libraries/src/integrations/social/linkedin.page.provider.ts
+++ libraries/nestjs-libraries/src/integrations/social/linkedin.page.provider.ts
@@ -145,7 +145,7 @@ export class LinkedinPageProvider
     id: string,
     requiredId: string,
     accessToken: string
-  ): Promise<AuthTokenDetails> {
+  ): Promise<Omit<AuthTokenDetails, 'refreshToken' | 'expiresIn'>> {
     const information = await this.fetchPageInformation(accessToken, {
       page: requiredId,
     });
@@ -154,8 +154,6 @@ export class LinkedinPageProvider
       id: information.id,
       name: information.name,
       accessToken: information.access_token,
-      refreshToken: information.access_token,
-      expiresIn: dayjs().add(59, 'days').unix() - dayjs().unix(),
       picture: information.picture,
       username: information.username,
     };
