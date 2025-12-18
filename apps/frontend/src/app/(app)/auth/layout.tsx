5896e3f8a37f94282537a803b1ce254d78c67f94
Nevo David <me@nevos.io>
2025-12-18 14:27:57 +0700

    feat: branded signin-signup

--- apps/frontend/src/app/(app)/auth/layout.tsx
+++ apps/frontend/src/app/(app)/auth/layout.tsx
@@ -7,6 +7,7 @@ import loadDynamic from 'next/dynamic';
 import { isGeneralServerSide } from '@gitroom/helpers/utils/is.general.server.side';
 import { LoginLeftSection } from '@gitroom/frontend/components/auth/login.left.section';
 import { Testimonial } from '@gitroom/frontend/components/auth/testimonial';
+import { TestimonialComponent } from '@gitroom/frontend/components/auth/testimonial.component';
 const ReturnUrlComponent = loadDynamic(() => import('./return.url.component'));
 export default async function AuthLayout({
   children,
@@ -17,7 +18,7 @@ export default async function AuthLayout({
 
   return (
     <div className="bg-[#0E0E0E] flex flex-1 p-[12px] gap-[12px] min-h-screen w-screen text-white">
-      <style>{`html, body {overflow-x: hidden;}`}</style>
+      {/*<style>{`html, body {overflow-x: hidden;}`}</style>*/}
       <ReturnUrlComponent />
       <div className="" />
       <div className="flex justify-center  items-center p-5 h-[100vh] w-[100vw] bg-loginBg bg-cover bg-no-repeat bg-center overflow-hidden">
@@ -65,47 +66,14 @@ export default async function AuthLayout({
           <div className="flex">{children}</div>
         </div>
       </div>
-      <div className="text-[36px] flex-1 pt-[88px] flex flex-col items-center">
+      <div className="text-[36px] flex-1 pt-[88px] hidden lg:flex flex-col items-center">
         <div className="text-center">
           Over <span className="text-[42px] text-[#FC69FF]">18,000+</span>{' '}
-          Entrepreneurs use<br />Postiz
-          To Grow Their Social Presence
+          Entrepreneurs use
+          <br />
+          Postiz To Grow Their Social Presence
         </div>
-        <div className="flex-1 relative w-full mt-[30px] overflow-hidden">
-          <div className="absolute gap-[12px] w-full h-full left-0 top-0 overflow-hidden flex justify-center px-[40px]">
-            <div className="absolute w-full h-[120px] left-0 top-0 blackGradTopBg z-[100]" />
-            <div className="absolute w-full h-[120px] left-0 bottom-0 blackGradBottomBg z-[100]" />
-            <div className="flex flex-col h-full animate-marqueeUp gap-[12px] flex-1">
-              {[1, 2].map((p) => (
-                <div key={p} className="flex flex-col gap-[12px]">
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                </div>
-              ))}
-            </div>
-            <div className="flex flex-col h-full animate-marqueeDown gap-[12px] flex-1">
-              {[1, 2].map((p) => (
-                <div key={p} className="flex flex-col gap-[12px]">
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                  <Testimonial />
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
+        <TestimonialComponent />
       </div>
     </div>
   );
